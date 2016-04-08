3d40c6e335ec7429eec063d3838b11125695079b
dam128 <dam128@PC2017.aulas.local>
2016-04-08 10:45:00 +0200

    primer commit.

---
+++ src/libreriapruebagit/LibreriaPruebaGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreriapruebagit;
+
+/**
+ *
+ * @author dam128
+ */
+public class LibreriaPruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/libreriapruebagit/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreriapruebagit;
+
+/**
+ *
+ * @author dam128
+ */
+public class Persona {
+    String nombre;
+}
